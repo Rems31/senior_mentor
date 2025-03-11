2404d0a6c9283de7772ae9fa2d5627fba4ddf54b
Remi Etaix Bonnin <remi.etaix.bonnin@gmail.com>
2025-03-11 16:43:21 +0100

    Modified booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,15 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_booking, only: %i[show edit update destroy]
+  before_action :authorize_booking_owner, only: %i[edit update destroy]
+
+  def show
+  end
+
+  def create
+    @offer = Offer.find(params[:offer_id])
+    @booking = Booking.new(user: current_user, offer: @offer, status: "pending", date: params[:booking][:date])
+
+    if @booking.save
+      redirect_to booking_path(@booking), 
 end
