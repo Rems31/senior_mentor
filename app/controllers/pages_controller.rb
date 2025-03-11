428a2eb1ab193f025b560be017e4aac7edeff0d2
Emilien Danielou <danielou.emilien@gmail.com>
2025-03-11 18:17:03 +0100

    created profile controller & view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
-  def home
+  skip_before_action :authenticate_user!, only: %i[profile]
+
+  def profile
+    @offers = current_user.offers
+    @bookings = current_user.bookings
   end
 end
