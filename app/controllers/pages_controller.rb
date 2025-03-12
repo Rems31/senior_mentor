a6b48107e858513d9c05d33f19ffabd499702fb8
Emilien Danielou <danielou.emilien@gmail.com>
2025-03-12 15:03:23 +0100

    edited profile controller & view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[profile]
 
   def profile
-    @offers = current_user.offers
-    @bookings = current_user.bookings
+    @my_offers = current_user.offers
+    @my_bookings = current_user.bookings
   end
 end
