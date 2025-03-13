82b1589400b0c482f78cf23007dfb999de19b306
Remi Etaix Bonnin <remi.etaix.bonnin@gmail.com>
2025-03-13 16:20:34 +0100

    Updated the booking controller and view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -12,14 +12,22 @@ class BookingsController < ApplicationController
 
   def create
     @offer = Offer.find(params[:offer_id])
+
+    # Vérifier si l'utilisateur tente de réserver sa propre offre
+    if @offer.user == current_user
+      redirect_to offer_path(@offer), alert: "You can't book your own offer!"
+      return
+    end
+
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.offer = @offer
     @booking.status = "pending"
 
     if @booking.save
-      redirect_to booking_path(@booking), notice: "Booking made with success."
+      redirect_to offer_path(@offer), notice: "Booking made with success."
     else
+      flash.now[:alert] = @booking.errors.full_messages.join(", ") # Afficher les erreurs
       render "offers/show", status: :unprocessable_entity
     end
   end
@@ -28,10 +36,13 @@ class BookingsController < ApplicationController
   end
 
   def update
-    if @booking.update(booking_params)
-      redirect_to booking_path(@booking), notice: "Booking updated."
+    if params[:status].present? && @booking.offer.user == current_user
+      @booking.update(status: params[:status])
+      redirect_to profile_path, notice: "Booking #{params[:status]}."
+    elsif @booking.user == current_user
+      redirect_to profile_path, alert: "You cannot change the status of your own booking."
     else
-      render :edit, status: :unprocessable_entity
+      redirect_to profile_path, alert: "You are not authorized."
     end
   end
 
@@ -47,7 +58,7 @@ class BookingsController < ApplicationController
   end
 
   def authorize_booking_owner
-    redirect_to root_path, alert: "You are not authorized." unless @booking.user == current_user
+    redirect_to root_path, alert: "You are not authorized." unless @booking.offer.user == current_user
   end
 
   def booking_params
